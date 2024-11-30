9b4e94a9b1e14a31ee26252cd3f89cd5193db7fd
Bhupendra <bhupendra.raj003.github@gmail.com>
2024-11-30 12:55:25 +0530

    updated server.js and package.json for netlify

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = require('../app.js');
+const path = require('path');
+
+// Set the views directory for the serverless function
+app.set('views', path.join(__dirname, '../views'));
 
 module.exports.handler = serverless(app);
